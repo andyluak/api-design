7c803bb9bbd945181e7ea2906974c2ed94244646
Alex Tirim <49715292+andyluak@users.noreply.github.com>
2022-11-02 22:06:10 +0200

    User / Product CRUD app

--- express-server/src/modules/auth.ts
+++ express-server/src/modules/auth.ts
@@ -2,35 +2,34 @@ import jwt from "jsonwebtoken";
 import * as bcrypt from "bcrypt";
 
 export const createJWT = ({ id, username }) => {
-	return jwt.sign({ id, username }, process.env.SECRET);
+  return jwt.sign({ id, username }, process.env.SECRET);
 };
 
 export const protect = (req, res, next) => {
 	const bearer = req.headers.authorization;
 
-	if (!bearer) {
-		res.status(401);
-		return res.send({ message: "Not authorized" });
-	}
+  if (!bearer) {
+    res.status(401);
+    return res.send({ message: "Not authorized" });
+  }
+  const [, token] = bearer.split(" ");
 
-	const [, token] = bearer.split(" ");
-
-	try {
-		const payload = jwt.verify(token, process.env.SECRET);
-		req.user = payload;
-		console.log(payload);
-		return next();
-	} catch (e) {
-		console.log(e);
-		res.status(401);
-		return res.send({ message: "Not authorized" });
-	}
+  try {
+    const payload = jwt.verify(token, process.env.SECRET);
+    req.user = payload;
+    console.log(payload);
+    return next();
+  } catch (e) {
+    console.log(e);
+    res.status(401);
+    return res.send({ message: "Not authorized" });
+  }
 };
 
 export const comparePasswords = (password, hash) => {
-	return bcrypt.compare(password, hash);
+  return bcrypt.compare(password, hash);
 };
 
 export const hashPassword = (password) => {
-	return bcrypt.hash(password, 5);
+  return bcrypt.hash(password, 5);
 };
--- express-server/src/router.ts
+++ express-server/src/router.ts
@@ -1,23 +1,51 @@
 import { Router } from "express";
-import { body } from "express-validator";
+import { body, param, query } from "express-validator";
+import prisma from "./db";
 import { handleInputErrors } from "./middleware";
+import { comparePasswords, hashPassword } from "./modules/auth";
 
 const router = Router();
 
+// extend the request type to include a user property
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any;
+    }
+  }
+}
+
 // Product routes
-router.get("/product", (req, res) => {
+router.get("/product", async (req, res) => {
+  const { id } = req.user;
+
+  const products = await prisma.product.findMany({
+    where: {
+      belongsToId: id,
+    },
+  });
   res.status(200);
 
-  res.json({ message: "Hello from products" });
+  res.json(products);
 });
 router.get("/product/:id", () => {});
 router.post(
   "/product",
-  body("name").isString(),
+  body("name").exists().isString(),
   handleInputErrors,
-  (req, res) => {
+  async (req, res) => {
+    const {
+      body: { name },
+      user: { id },
+    } = req;
+    const product = await prisma.product.create({
+      data: {
+        belongsToId: id,
+        name,
+      },
+    });
     res.status(200);
-    res.json({ message: "Hello from products" });
+    res.json({ product });
   }
 );
 router.put(
@@ -32,10 +60,52 @@ router.put(
 router.patch(
   "/product/:id",
   body("name").isString(),
+  param("id").exists().isString(),
   handleInputErrors,
-  (req, res) => {}
+  async (req, res) => {
+    const {
+      body: { name },
+      params: { id },
+    } = req;
+
+    try {
+      await prisma.product.update({
+        where: {
+          id,
+        },
+        data: {
+          name,
+        },
+      });
+      res.status(200);
+      res.json({ message: "Product Deleted" });
+    } catch (e) {
+      return res.status(401);
+    }
+  }
 );
-router.delete("/product/:id", () => {});
+router.delete(
+  "/product/:id",
+  param("id").exists().isString(),
+  handleInputErrors,
+  async (req, res) => {
+    const {
+      params: { id },
+      user,
+    } = req;
+    try {
+      await prisma.product.delete({
+        where: {
+          id,
+        },
+      });
+      res.status(200);
+      res.json({ message: "Product Deleted" });
+    } catch (e) {
+      return res.status(401);
+    }
+  }
+);
 
 // Update routes
 router.get("/update", (req, res) => {});
@@ -83,5 +153,46 @@ router.put(
 );
 router.patch("/updatepoint/:id", () => {});
 router.delete("/updatepoint/:id", () => {});
+
+router.put(
+  "/user",
+  body(["password", "currentPassword"])
+    .isString()
+    .exists()
+    .isLength({ min: 8 }),
+  handleInputErrors,
+  async (req, res) => {
+    const {
+      body: { password, currentPassword },
+      //@ts-ignore
+      user: { username },
+    } = req;
+
+    const user = await prisma.user.findUnique({ where: { username } });
+    const isPasswordValid = await comparePasswords(
+      currentPassword,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      res.status(400);
+      res.json({ message: "incorrect password" });
+      return;
+    }
+
+    const hashedPassword = await hashPassword(password);
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        password: hashedPassword,
+      },
+    });
+
+    res.status(200);
+    res.json({ message: "Hello from products" });
+  }
+);
 
 export default router;
